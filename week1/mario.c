3af1fb870411849ebc12f037ca8b718d41c999c3
Enver Ibraimi <enver.ibraimi@ibu.edu.mk>
2026-05-09 22:16:22 +0200

    Add files via upload

--- week1/mario.c
+++ week1/mario.c
@@ -1,47 +1,24 @@
-// mario.c
-// CS50x - Week 1, Problem Set 1
-//
-// Exercise: Mario (Less Comfortable)
-// ------------------------------------
-// Print a right-aligned half-pyramid of # symbols.
-// The user provides the height (1–8).
-//
-// Expected output (height = 4):
-//    #
-//   ##
-//  ###
-// ####
-//
-// How to compile:  make mario
-// How to run:      ./mario
-// How to check:    check50 cs50/problems/2024/x/mario/less
-
-#include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int height;
+int main() {
+    int height ;
+    printf("Enter the height of the pyramid: ");
+    
+    
+    scanf("%d", &height); 
 
-    // TODO: Use a do-while loop to prompt for height.
-    //       Keep re-prompting until height is between 1 and 8 (inclusive).
-    do
-    {
-        // TODO: Get an integer from the user with prompt "Height: "
+    for (int i = 1; i <= height; i++) {
+        
+       
+        for (int j = 1; j <= height - i; j++) {
+            printf(" ");
+        
+  
+        for (int k = 1; k <= i; k++) {
+            printf("#");
+        }
 
-    }
-    while (/* TODO: condition to keep looping */ false);
-
-    // TODO: Use a for loop to iterate over each row (1 through height).
-    for (int row = 1; row <= height; row++)
-    {
-        // TODO: Print (height - row) spaces using a for loop.
-
-
-        // TODO: Print (row) hashes using a for loop.
-
-
-        // Print a newline to end the row.
+        
         printf("\n");
     }
 
